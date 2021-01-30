fdc3a1ee553c782bac6d7c497fc86ce0a346e9b9
Emin Mammedli <emincheater96@gmail.com>
2021-01-30 16:54:53 +0400

    no apple yet

--- snake.js
+++ snake.js
@@ -2,7 +2,7 @@ function Snake() {
 	// properties
 	this.x = 0;
 	this.y = 0;
-	this.xspeed = 1;
+	this.xspeed = 2;
 	this.yspeed = 0;
 	this.total = 0;
 	this.tail = [];
@@ -21,14 +21,10 @@ function Snake() {
 	}
 
 	this.update = function() {
-		for(var i = 0; i < this.total; i++) {
-			tail[i] = tail[i + 1];
-			tail[total-1] = createVector(this.x,this.y);
-			this.x = this.x + this.xspeed*scl;
-			this.y = this.y + this.yspeed*scl;
+			this.x = this.x + this.xspeed;
+			this.y = this.y + this.yspeed;
 			this.x= constrain(this.x, 0, width - scl);
 			this.y= constrain(this.y, 0, height - scl);
-		}
 	}
 	this.show = function() {
 			for(var i = 0; i < this.total; i++) {
